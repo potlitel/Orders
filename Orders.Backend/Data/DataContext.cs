821556c4f2a14d71bd7f363740e1743d3a98ca94
potlitel <potlitel@gmail.com>
2023-11-08 09:22:08 -0500

    Se adiciona entidad Country y se aplica primera migración.

---
+++ Orders.Backend/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿namespace Orders.Backend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
