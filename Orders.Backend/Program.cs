4c4ee0fe97ca0d314a8c2267cbf529533edc763b
potlitel <potlitel@gmail.com>
2023-11-08 09:29:55 -0500

    Se adiciona CRUD de paises desde proyecto backend.

--- Orders.Backend/Program.cs
+++ Orders.Backend/Program.cs
@@ -25,4 +25,10 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true)
+    .AllowCredentials());
+
 app.Run();
