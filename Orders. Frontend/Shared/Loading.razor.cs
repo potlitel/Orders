7b397b3190b04f06e5f52f2660f04dc1809e3372
potlitel <potlitel@gmail.com>
2023-11-08 15:53:27 -0500

    Se adiciona componente "Loading" y se incorpora sección "CountryDetails".

---
+++ Orders. Frontend/Shared/Loading.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Orders._Frontend.Shared
+{
+    public partial class Loading
+    {
+        public Loading()
+        {
+
+        }
+    }
+}
